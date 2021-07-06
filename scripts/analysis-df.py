8d126e6640cedcc913063c40c915665c2df756ac
Rachel Yang <ruoqi.yang@vanderbilt.edu>
2021-07-06 16:21:47 +0800

    merged motion column

--- scripts/analysis-df.py
+++ scripts/analysis-df.py
@@ -16,6 +16,7 @@ device_id = ""
 # MOTION = sys.argv[2:]
 
 DATAFILE = "4A@05-15.LOG"
+MOTION = ["4A@05-15-motion.csv", "50@05-15-motion.csv", "51@05-15-motion.csv"]
 MOTION1 = "4A@05-15-motion.csv"
 MOTION2 = "50@05-15-motion.csv"
 MOTION3 = "51@05-15-motion.csv"
@@ -72,58 +73,6 @@ other_device_col = 2
 dist_col = 3
 in_range_col = 4
 
-"""
-timestamp = []
-in_range = []
-
-for j in range(len(df_lst[0])):
-    timestamp.append(df_lst[0].iloc[[j], [timestamp_col]].values[0][0])
-    in_range.append(df_lst[0].iloc[[j], [in_range_col]].values[0][0])
-
-check_in = [-1 for i in range(len(timestamp))]
-
-index = 0
-check_num = 0
-
-
-while index in range(len(timestamp)):
-    print(check_num)
-    if in_range[index]:
-        t = int(timestamp[index])
-
-        count = 1
-        tmpT = t
-        i = index
-        first = True
-        add_to_beginning = False
-        check_num_update = False
-
-        while in_range[index + 1] and index != len(timestamp) - 1 and (tmpT + 1) == int(timestamp[index + 1]):
-            count = count + 1
-            if first:
-                track_beginning = i
-                first = False
-            if count >= 2:
-                if not check_num_update:
-                    check_num = check_num + 1
-                    check_num_update = True
-                check_in[index + 1] = check_num
-                if not add_to_beginning:
-                    check_in[track_beginning] = check_num
-                    add_to_beginning = True
-            index = index + 1
-            tmpT = tmpT + 1
-        index = index + 1
-    else:
-        index = index + 1
-
-df_lst[0]["check_in"] = check_in
-"""
-
-df_lst[0].to_csv("d1", sep="\t")
-# df_lst[1].to_csv("d2", sep="\t")
-# df_lst[2].to_csv("d3", sep="\t")
-
 
 for df in df_lst:
 
@@ -177,8 +126,44 @@ for df in df_lst:
 
     # append a new 'check_in' col to data frame
     df["check_in"] = check_in
+    print(len(timestamp), len(check_in))
 
 
 df_lst[0].to_csv("d1", sep="\t")
 df_lst[1].to_csv("d2", sep="\t")
 df_lst[2].to_csv("d3", sep="\t")
+
+whole = pd.concat(df_lst)
+whole = pd.DataFrame(whole).sort_values(by=['timestamp'])
+whole.dropna(axis=0, how='any', thresh=None, subset=None, inplace=False)
+
+# get all timestamps
+all_timestamp = []
+for i in range(len(whole.index)):
+    all_timestamp.append(whole.iloc[[i], [0]].values[0][0])
+
+motion_lst = []
+count = 1
+for motion_file in MOTION:
+    motion_lst.append(pd.read_csv(motion_file, comment="T",
+                                  names=["timestamp", "motion"+str(count), "Other_Device"], sep=","))
+    count += 1
+
+time = pd.DataFrame(all_timestamp, columns=["timestamp"])
+merged = whole
+count = 1
+for df in motion_lst:
+    df = pd.merge(time, df, on=["timestamp"], how="outer")
+    df.fillna(method="ffill", inplace=True)
+    del df["Other_Device"]
+    df = df.drop_duplicates(subset=['timestamp']).dropna(subset=['motion'+str(count)])
+    df = pd.DataFrame.astype(self=df, dtype=int)
+    merged = pd.merge(merged, df, on=['timestamp'])
+    count += 1
+
+
+merged.to_csv("merged", sep="\t")
+print(len(df_lst[0].index))
+print(len(df_lst[1].index))
+print(len(df_lst[2].index))
+print(len(whole.index))
